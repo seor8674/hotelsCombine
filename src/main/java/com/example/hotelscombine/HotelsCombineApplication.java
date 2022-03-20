6b5b3349da664c8760896df9d6678268717bbe33
leehwanjun <hwanjuni70@gmail.com>
2022-03-20 19:47:15 +0900

    Initial commit

---
+++ src/main/java/com/example/hotelscombine/HotelsCombineApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelscombine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsCombineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsCombineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotelscombine/HotelsCombineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelscombine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsCombineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
